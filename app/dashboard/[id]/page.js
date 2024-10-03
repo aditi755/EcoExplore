1267d1eea5dfbd2b4f9c85c287ec0c5d6dc8672d
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-10-04 02:46:03 +0530

    commit this

--- app/dashboard/[id]/page.js
+++ app/dashboard/[id]/page.js
@@ -3,7 +3,7 @@ import Dashboard from '../../components/Dashboard';
 export default function DynamicDashboardPage() {
   return <Dashboard />;
 }
-
+//Improved Performance: These pages are pre-rendered at build time, resulting in faster page loads. Reduced Server Load: Since pages are pre-generated, there's less work for the server to do at request time.  SSG AND ISG
 export async function generateStaticParams() {
   return [
     { id: 'overview' },
